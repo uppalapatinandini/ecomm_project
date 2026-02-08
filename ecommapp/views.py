33bbcd31ad7060a909d28f5f4d51be9e35691388
uppalapatinandini <uppalapatinandini16@gmail.com>
2026-02-09 00:24:17 +0530

    ur_backup

--- ecommapp/views.py
+++ ecommapp/views.py
@@ -19,7 +19,7 @@ def register_view(request):
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password')
-        confirm_password = request.POST.get('confirm_password')
+        confirm_password = request.POST.get('confirm_password') 
 
         # Validation
         if password != confirm_password:
